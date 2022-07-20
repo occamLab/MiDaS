979d033bbdde67688e2821496c9f3c045fb686ec
Kenta Burpee <kentaburpee@gmail.com>
2022-07-20 15:02:36 -0400

    Start filtering point clouds

--- mobile/ios/Midas/HandlePointCloud.swift
+++ mobile/ios/Midas/HandlePointCloud.swift
@@ -16,9 +16,22 @@ func getTrueLidarPointCloud(logFrame: ARFrameDataLog) -> [simd_float3] {
 
 func getGlobalPointCloud(logFrame: ARFrameDataLog, truePointCloud: [simd_float3]) -> [simd_float3] {
     let pose = logFrame.pose
-    var globalPointCloud = truePointCloud.map({ point in pose * simd_float4(point, 1) })
+    let globalPointCloud = truePointCloud.map({ point in pose * simd_float4(point, 1) })
     let theta = atan2(pose[0][2], pose[2][2])
     let yAxis = simd_float3(0, 1, 0)
     let rotationMatrix = float4x4(simd_quatf(angle: -theta, axis: yAxis))
-    // Multiply rotationMatrix and globalPointCloud
+    let yawAdjustedPointCloud = globalPointCloud.map({ point in rotationMatrix * point })
+    return yawAdjustedPointCloud.map({point in [point[0], point[1], point[2]]})
+}
+
+func isolateObstacles(logFrame: ARFrameDataLog, yawAdjustedPointCloud: [simd_float3]) -> [simd_float3] {
+    let depthOffset = yawAdjustedPointCloud.map({ point in point[2]}).max()
+    var filteredPointCloud = yawAdjustedPointCloud.map({ point in [point[0], point[1], point[2] - depthOffset!]})
+    filteredPointCloud = filteredPointCloud.filter{$0[2] >= -4}
+    let xValues = filteredPointCloud.map({ point in point[0]})
+    let xOffset = (xValues.max()! + xValues.min()!) / 2
+    let yOffset = filteredPointCloud.map({point in point[1]}).min()
+    filteredPointCloud = filteredPointCloud.map({ point in [point[0] - xOffset, point[1] - yOffset!, point[2]]})
+    filteredPointCloud = filteredPointCloud.filter{$0[0] <= 1 && $0[1] > 0.25}
+    return filteredPointCloud
 }
