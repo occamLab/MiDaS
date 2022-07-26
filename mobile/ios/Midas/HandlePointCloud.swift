4e62647516a79caa73b6f4fedc86a4038369def1
Kenta Burpee <kentaburpee@gmail.com>
2022-07-26 10:11:36 -0400

    Order of operations

--- mobile/ios/Midas/HandlePointCloud.swift
+++ mobile/ios/Midas/HandlePointCloud.swift
@@ -37,7 +37,7 @@ func isolateObstacles(logFrame: ARFrameDataLog, yawAdjustedPointCloud: [simd_flo
     var filteredPointCloud = yawAdjustedPointCloud.map({ point in simd_float3(point[0], point[1], point[2] - depthOffset!)})
     filteredPointCloud = filteredPointCloud.filter{$0[2] >= -4}
     let xValues = filteredPointCloud.map({ point in point[0]})
-    let xOffset = (xValues.max() ?? 0 + (xValues.min() ?? 0)) / 2
+    let xOffset = ((xValues.max() ?? 0) + (xValues.min() ?? 0)) / 2
     let yOffset = filteredPointCloud.map({point in point[1]}).min()
     filteredPointCloud = filteredPointCloud.map({ point in simd_float3(point[0] - xOffset, point[1] - (yOffset ?? 0), point[2])})
     filteredPointCloud = filteredPointCloud.filter{abs($0[0]) <= 0.5 && $0[1] > 0.25}
